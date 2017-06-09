43ee2f66c696082155632badc83a02b2f7a55cc2
Marco Wessel <marco@poop.nl>
2017-06-09 13:16:34 +0000

    Enable Enigma plugin for GPG

--- copy/root/config.inc.php
+++ copy/root/config.inc.php
@@ -6,11 +6,13 @@ $config['log_driver'] = 'syslog';
 $config['syslog_facility'] = LOG_MAIL;
 $config['product_name'] = 'Mail';
 
-$config['plugins'] = array('acl', 'managesieve');
+$config['plugins'] = array('acl', 'managesieve', 'enigma');
 $config['spellcheck_engine'] = 'atd';
 $config['htmleditor'] = 2;
 $config['mdn_requests'] = 2;
 
+$config['enigma_pgp_homedir'] = "/srv/roundcube/data";
+$config['enigma_password_time'] = 15;
 
 @include("/opt/local/roundcube/config/config.local.inc.php");
 @include("/srv/roundcube/config/config.local.inc.php");
